4505db5df264f0cc2c27d0507ef111e61fb0e17b
Brayan Rivas <brayan12345303@gmail.com>
2023-09-18 20:37:39 -0600

    Cambios

--- TSS/FrontEnd/Helpers/ServiceRepository.cs
+++ TSS/FrontEnd/Helpers/ServiceRepository.cs
@@ -19,18 +19,22 @@
         //    Client.DefaultRequestHeaders.Authorization =
         //      new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
         //}
+
         public HttpResponseMessage GetResponse(string url)
         {
             return Client.GetAsync(url).Result;
         }
+
         public HttpResponseMessage PutResponse(string url, object model)
         {
             return Client.PutAsJsonAsync(url, model).Result;
         }
+
         public HttpResponseMessage PostResponse(string url, object model)
         {
             return Client.PostAsJsonAsync(url, model).Result;
         }
+
         public HttpResponseMessage DeleteResponse(string url)
         {
             return Client.DeleteAsync(url).Result;
