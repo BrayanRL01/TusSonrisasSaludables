a558b53f530707ab5d402cdd1f7b4efd5beddc71
Brayan Rivas <brayan12345303@gmail.com>
2023-09-18 19:44:31 -0600

    Cambio en el UserViewModel

--- TSS/FrontEnd/Models/UserViewModel.cs
+++ TSS/FrontEnd/Models/UserViewModel.cs
@@ -10,6 +10,7 @@ namespace FrontEnd.Models
         public int TypeId { get; set; }
         public int GenreId { get; set; }
         public int ProvinceId { get; set; }
+        [StringLength(11, ErrorMessage = "El número de cédula consta de 11 carácteres.")]
         [RegularExpression(@"^[1-8]-[0-9]{4}-[0-9]{4}$",
         ErrorMessage = "El número de cédula debe tener el siguiente formato: 1-1111-1111")]
         public string Idnumber { get; set; } = null!;
@@ -20,7 +21,7 @@ namespace FrontEnd.Models
         public DateTime BirthDate { get; set; }
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = null!;
-
+        [StringLength(9, ErrorMessage = "El número de teléfono consta de 9 carácteres.")]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^[2, 5, 6, 7, 8][0-9]{3}-[0-9]{4}$",
         ErrorMessage = "El número de teléfono debe tener el siguiente formato: 8888-8888")]
