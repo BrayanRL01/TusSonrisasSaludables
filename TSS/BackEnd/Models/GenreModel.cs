688c1e1f54f95915c6e69077ffbfec069f7c84ba
Brayan Rivas <brayan12345303@gmail.com>
2023-09-18 19:29:19 -0600

    Se agregan datos de algunos Models

--- TSS/BackEnd/Models/GenreModel.cs
+++ TSS/BackEnd/Models/GenreModel.cs
@@ -2,5 +2,7 @@
 {
     public class GenreModel
     {
+        public int GenreId { get; set; }
+        public string GenreName { get; set; } = null!;
     }
 }
--- TSS/BackEnd/Models/ProvinceModel.cs
+++ TSS/BackEnd/Models/ProvinceModel.cs
@@ -2,5 +2,7 @@
 {
     public class ProvinceModel
     {
+        public int ProvinceId { get; set; }
+        public string ProvinceName { get; set; } = null!;
     }
 }
