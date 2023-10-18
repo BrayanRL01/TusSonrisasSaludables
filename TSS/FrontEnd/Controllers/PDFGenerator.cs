bbc1fa992b17e1a846bd8d49aec62e9d8e87a7b3
Valery <vovares80435@ufide.ac.cr>
2023-10-18 13:46:31 -0600

    reporte

---
+++ TSS/FrontEnd/Controllers/PDFGenerator.cs
@@ -0,0 +1,32 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+
+    public class PDFGenerator
+    {
+        private IConverter _converter;
+
+        public PDFGenerator(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        public byte[] GeneratePdf(string htmlContent)
+        {
+            var document = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                PaperSize = PaperKind.A4,
+                Orientation = Orientation.Portrait,
+            },
+                Objects = {
+                new ObjectSettings()
+                {
+                    PagesCount = true,
+                    HtmlContent = htmlContent
+                }
+            }
+            };
+
+            return _converter.Convert(document);
+        }
+    }
