a7c8e393269077ed527e170added73204c8f3ea1
Brayan Rivas <brayan12345303@gmail.com>
2023-11-07 01:46:29 -0600

    Deploy en Azure y cambio en Program de BE

--- TSS/BackEnd/Program.cs
+++ TSS/BackEnd/Program.cs
@@ -76,6 +76,17 @@ builder.Services.AddSwaggerGen(option =>
 
 var app = builder.Build();
 
+app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
